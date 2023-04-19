14555c5a2cc8733818a3583349fd9eaa31ed6b6b
devopsdj <michael@blue360media.com>
2023-04-18 22:44:55 -0700

    3rd commit?

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>amplify!</h1>
+      <h1>amplify!!</h1>
     </div>
   );
 }
